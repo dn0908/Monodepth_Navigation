106b829ec5e4c8fedfe6cd604be78848cfb8ead4
dn0908 <dahyun.diana.nam@gmail.com>
2024-12-03 21:32:06 +0900

    minor changes (debugging @ coral dev board)

--- cameraDepthEstimation.py
+++ cameraDepthEstimation.py
@@ -23,32 +23,23 @@ out_video = cv2.VideoWriter(
     )
 
 while True:
-
-	# Read frame from the webcam
-	ret, img = camera.read()	
-    
+    # Read frame from the webcam
+    ret, img = camera.read()	
     ###################################################################
     inference_start_time = time.perf_counter()
-
     # Estimate depth
 	colorDepth = depthEstimator.estimateDepth(img)
-    
     inference_stop_time = time.perf_counter()
     inference_duration = inference_stop_time - inference_start_time
     print("Inference time : ", inference_duration, "s")
     total_inference_duration += inference_duration
     ###################################################################
-
 	# Add the depth image over the color image:
 	combinedImg = cv2.addWeighted(img,0.7,colorDepth,0.6,0)
-
 	# Join the input image, the estiamted depth and the combined image
 	img_out = np.hstack((img, colorDepth, combinedImg))
-
     out_video.write(img_out)
-
 	# cv2.imshow("Depth Image", img_out)
-
 	# Press key q to stop
 	if cv2.waitKey(1) == ord('q'):
 		break
