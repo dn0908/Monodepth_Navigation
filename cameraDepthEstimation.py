0e40f17d1e2635cc6ee43a7b8d3b1ea52bc60520
dn0908 <dahyun.diana.nam@gmail.com>
2024-12-03 21:30:57 +0900

    minor changes

--- cameraDepthEstimation.py
+++ cameraDepthEstimation.py
@@ -29,7 +29,7 @@ while True:
     
     ###################################################################
     inference_start_time = time.perf_counter()
-    
+
     # Estimate depth
 	colorDepth = depthEstimator.estimateDepth(img)
     
@@ -48,7 +48,7 @@ while True:
     out_video.write(img_out)
 
 	# cv2.imshow("Depth Image", img_out)
-    
+
 	# Press key q to stop
 	if cv2.waitKey(1) == ord('q'):
 		break
